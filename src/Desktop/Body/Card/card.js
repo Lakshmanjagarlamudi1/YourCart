3346d8be89e42ce5bb2a5860fb30ea2c1fa29454
Jagarlamudi lakshmi Narayana <jagarlamudilakshman1233@gmail.com>
2021-10-16 20:13:52 +0530

    commit

--- src/Desktop/Body/Card/card.js
+++ src/Desktop/Body/Card/card.js
@@ -8,19 +8,19 @@ const CardUi = () => {
     return (
         <div className="crad-ui">
             <Card style={{ width: '18rem' }}>
-                {CardData.map((item, index) => {
-                    return (
-                        <div className="card-ui-a">
-                            {item.image}
-                            <Card.Body>
-                                <Card.Title>{item.title}</Card.Title>
-                                <Card.Text>{item.text}</Card.Text>
-                                <Button variant="primary">{item.button}</Button>
-                            </Card.Body>
-                        </div>
-                    )
-                })}
-            </Card>
+                        {CardData.map((item, index) => {
+                            return (
+                                <div className={item.cName}>
+                                    {item.image}
+                                    <Card.Body>
+                                        <Card.Title className="card-title">{item.title}</Card.Title>
+                                        <Card.Text>{item.text}</Card.Text>
+                                        <Button variant="primary">{item.button}</Button>
+                                    </Card.Body>
+                                </div>
+                            )
+                        })}
+                    </Card>
         </div>
     )
 }
--- src/Desktop/Body/body.js
+++ src/Desktop/Body/body.js
@@ -1,32 +1,17 @@
 import React from 'react'
 import SideNav from './SideNav/sideNav'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import { CardData } from './Card/cardData'
 import './body.css'
+import CardUi from './Card/card'
 
 const Body = () => {
     return (
         <div className="body-ui">
             <div className="body-left">
                 <SideNav />
-                <div className="body-right">
-                    <div className="card">
-                    <Card style={{ width: '18rem' }}>
-                        {CardData.map((item, index) => {
-                            return (
-                                <div className={item.cName}>
-                                    {item.image}
-                                    <Card.Body>
-                                        <Card.Title className="card-title">{item.title}</Card.Title>
-                                        <Card.Text>{item.text}</Card.Text>
-                                        <Button variant="primary">{item.button}</Button>
-                                    </Card.Body>
-                                </div>
-                            )
-                        })}
-                    </Card>
-                    </div>
+            </div>
+            <div className="body-right">
+                <div className="card">
+                    <CardUi />
                 </div>
             </div>
         </div>
