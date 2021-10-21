8eb2562507a0b18130f80b83caa39be01f9964a3
Jagarlamudi lakshmi Narayana <jagarlamudilakshman1233@gmail.com>
2021-10-21 13:15:45 +0530

    sideNavBar

--- src/Desktop/Body/SideNav/sideNav.js
+++ src/Desktop/Body/SideNav/sideNav.js
@@ -1,12 +1,31 @@
-import { Link } from '@material-ui/core'
-import React from 'react'
+import { Link, Button } from '@material-ui/core'
+import React, { useState } from 'react'
+import * as FiIcons from 'react-icons/fi';
 import { SidenavBardata } from './sidenavBardata'
 import './SideNav.css'
 
 const SideNav = () => {
+
+    const [nameClass, setNameClass] = useState(false)
+
     return (
         <div>
-            <ul className="side-nav-lists">
+            <Button
+                style={{
+                    marginLeft: '-20px',
+                    display: nameClass ? 'block' : 'none'
+                }}
+                onClick={() => setNameClass(!nameClass)}>
+                <FiIcons.FiChevronsRight
+                    style={{
+                        color: "#242B2E",
+                        fontSize: "20px"
+                    }} /></Button>
+
+            <ul style={{
+                marginLeft: '-50px',
+                display: nameClass ? 'none' : 'block'
+            }}>
                 {SidenavBardata.map((item, index) => (
                     <li key={index} className={item.cName}>
                         <Link className="links" href={item.path}>
@@ -15,8 +34,14 @@ const SideNav = () => {
                         </Link>
                     </li>
                 ))}
+                <Button
+                    onClick={() => setNameClass(!nameClass)}>
+                    <FiIcons.FiChevronsLeft
+                        style={{
+                            color: "#242B2E",
+                            fontSize: "20px"
+                        }} /></Button>
             </ul>
-
         </div>
     )
 }
